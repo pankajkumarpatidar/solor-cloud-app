c9d57221700380d4686716ba445cd7841fe03e24
Pankaj Patel <kumarpankajpatidar@gmail.com>
2026-03-03 16:33:00 +0530

    fixed register and login structure + jwt added

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const app = express();
 const bcrypt = require("bcryptjs");
 const User = require("./models/User");
+const jwt = require("jsonwebtoken");
+
 app.use(cors());
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
@@ -13,9 +15,37 @@ mongoose.connect(process.env.MONGO_URI)
 app.get("/", (req, res) => {
   res.send("NEW VERSION LIVE 🔥");
 });
+// ================= REGISTER =================
 app.post("/register", async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists ❌" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+      role
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully ✅" });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
+// ================= LOGIN =================
 app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -30,8 +60,16 @@ app.post("/login", async (req, res) => {
       return res.status(400).json({ message: "Wrong password ❌" });
     }
 
+    // 🔐 JWT Token Generate
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
     res.json({
       message: "Login successful ✅",
+      token,
       user: {
         id: user._id,
         name: user.name,
