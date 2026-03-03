8642afb653620acf85295893f6c67fe1f168724d
Pankaj Patel <kumarpankajpatidar@gmail.com>
2026-03-03 15:15:24 +0530

    register api added

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
-
+const bcrypt = require("bcryptjs");
+const User = require("./models/User");
 app.use(cors());
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
@@ -12,8 +13,37 @@ mongoose.connect(process.env.MONGO_URI)
 app.get("/", (req, res) => {
   res.send("Solar Cloud App Running 🚀");
 });
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
 
-const PORT = 5000;
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new user
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+      role
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully ✅" });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
